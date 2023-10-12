69e0813c80acf80e6e17e6f3e9ace29b09849cfe
yewon1621 <48310151+yewon1621@users.noreply.github.com>
2023-10-12 16:18:23 +0900

    Add files via upload

---
+++ aa04-rpt06/wk05_src/AAnn_tmp36_start/AAnn_tmp36_start.ino
@@ -0,0 +1,35 @@
+//
+// AA00, TMP36 sensor
+//
+
+#define TEMP_INPUT 0
+// or  int TEMP_INPUT = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+   //getting the voltage reading from the temperature sensor
+  int value = analogRead(TEMP_INPUT);
+  // Serial.print("value:");
+  // Serial.print(value);
+  // Serial.print(" , voltage:");
+ 
+   // converting that reading to voltage
+  float voltage = value * 5.0 * 1000.0;  // in mV
+  voltage /= 1023.0;
+   
+  // print out the voltage
+  // Serial.print(voltage); 
+  // Serial.print(" mV, ");
+   
+   // now print out the temperature
+  
+  // Serial.print("Temperature:");
+  float temperatureC = (voltage - 500) / 10 ;  
+  Serial.println(temperatureC); 
+  // Serial.println(" degrees C");
+
+  delay(1000);
+}
