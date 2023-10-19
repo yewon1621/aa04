dc1547fbb762100b4d4c926067e52942346b093f
yewon1621 <48310151+yewon1621@users.noreply.github.com>
2023-10-19 16:19:57 +0900

    Add files via upload

---
+++ aa04-rpt06/AA04_tmp36_cds.ino/AA04_tmp36_cds.ino.ino
@@ -0,0 +1,32 @@
+#define TMP36_INPUT 0
+#define CDS_INPUT 1
+// or  int TEMP_INPUT = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int temp_value = analogRead(TMP36_INPUT);
+  
+  // converting that reading to voltage
+  float voltage = temp_value * 5.0 * 1000.0;  // in mV
+  voltage /= 1023.0;
+  float tempC = (voltage - 500) / 10 ;  
+
+  int cds_value = analogRead(CDS_INPUT);
+  int lux = int(luminosity(cds_value));
+ 
+  Serial.print(tempC);
+  Serial.print(",");
+  Serial.println(lux);
+
+  delay(1000);
+}
+
+double luminosity(int RawADC0){
+  double Vout=RawADC0 * 5.0 / 1023.0;
+  int lux = (2500/Vout-500)/10;
+  
+  return lux;
+ }
